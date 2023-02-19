d463ee2190a8e2f19fe2c6232ea1b9e34d381e2d
Aephir <waldenb@gmail.com>
2023-02-19 10:15:18 +0100

    Fixed __init__.py

--- custom_components/test_sensor/const.py
+++ custom_components/test_sensor/const.py
@@ -1,4 +1,13 @@
 DOMAIN = "test_sensor"
 CONF_ENTITY = "entity"
 CONF_NAME = "name"
-
+STARTUP = """
+################################
+#                              #
+# This is a test integration   #
+# It does nothing useful,      #
+# except teach me how to write #
+# integrations.                #
+#                              #
+################################
+"""
